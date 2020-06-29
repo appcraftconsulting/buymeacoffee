11f7992eed430ddac15b713838fad9e79ff112b7
François Boulais <fboulais@me.com>
2020-06-29 07:49:27 +0200

    Add resources

--- Package.swift
+++ Package.swift
@@ -23,7 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "buymeacoffee",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Sources/img.pdf")
+            ]),
         .testTarget(
             name: "buymeacoffeeTests",
             dependencies: ["buymeacoffee"]),
--- Sources/buymeacoffee/BMCButton.swift
+++ Sources/buymeacoffee/BMCButton.swift
@@ -23,8 +23,27 @@ class BMCButton: UIButton {
             
             static let `default`: Self = .orange
             
-            var background: UIColor? {
-                UIColor(named: rawValue)
+            var background: UIColor {
+                switch self {
+                case .orange:
+                    return #colorLiteral(red: 1, green: 0.5059999824, blue: 0.2469999939, alpha: 1)
+                case .yellow:
+                    return #colorLiteral(red: 1, green: 0.8669999838, blue: 0, alpha: 1)
+                case .purple:
+                    return #colorLiteral(red: 0.7409999967, green: 0.3729999959, blue: 1, alpha: 1)
+                case .black:
+                    return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                case .white:
+                    return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                case .blue:
+                    return #colorLiteral(red: 0.3729999959, green: 0.4979999959, blue: 1, alpha: 1)
+                case .green:
+                    return #colorLiteral(red: 0.474999994, green: 0.8389999866, blue: 0.7099999785, alpha: 1)
+                case .red:
+                    return #colorLiteral(red: 1, green: 0.3729999959, blue: 0.3729999959, alpha: 1)
+                case .pink:
+                    return #colorLiteral(red: 0.9570000172, green: 0.4429999888, blue: 1, alpha: 1)
+                }
             }
             
             var title: UIColor {
@@ -42,7 +61,7 @@ class BMCButton: UIButton {
             case lato
             case arial
             
-            static let `default`: Self = .cookie
+            static let `default`: Self = .arial
             
             var value: UIFont? {
                 switch self {
@@ -64,14 +83,21 @@ class BMCButton: UIButton {
     
     var presentingViewController: UIViewController?
     var delegate: BMCButtonDelegate?
+    
     var configuration = Configuration() {
         didSet {
             configure(with: configuration)
         }
     }
     
+    override var isHighlighted: Bool {
+        didSet {
+            alpha = isHighlighted ? 0.85 : 1
+        }
+    }
+    
     convenience init(configuration: Configuration) {
-        self.init(frame: .zero)
+        self.init(type: .custom)
         
         self.configuration = configuration
     }
@@ -86,6 +112,15 @@ class BMCButton: UIButton {
         super.init(coder: coder)
         
         setup()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let rect = bounds.insetBy(dx: 5, dy: 5)
+        let cornerRadii = CGSize(width: 5, height: 5)
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: .allCorners, cornerRadii: cornerRadii)
+        layer.shadowPath = path.cgPath
     }
     
     // MARK: - Interface Builder
@@ -106,8 +141,13 @@ class BMCButton: UIButton {
     }
     
     private func setup() {
+        layer.shadowOffset = .init(width: 0, height: 1)
+//        layer.shadowColor = #colorLiteral(red: 0.7450980392, green: 0.7450980392, blue: 0.7450980392, alpha: 0.005).cgColor
+        layer.shadowOpacity = 1
+        layer.shadowRadius = 1
         layer.cornerRadius = 5
-        
+
+        contentEdgeInsets = .init(top: 7, left: 15, bottom: 7, right: 10)
         configure(with: configuration)
     }
 }
