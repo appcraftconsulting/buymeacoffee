9007634207067006e1663c05d54e8cece0660cdf
Alberto Garcia <45308839+AlbGarciam@users.noreply.github.com>
2021-03-13 11:19:00 +0100

    Support dynamic library

--- Package.swift
+++ Package.swift
@@ -10,9 +10,8 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "BuyMeACoffee",
-            targets: ["BuyMeACoffee"]),
+        .library(name: "BuyMeACoffee", targets: ["BuyMeACoffee"]),
+        .library(name: "BuyMeACoffeeDynamic", type: .dynamic, targets: ["BuyMeACoffee"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
